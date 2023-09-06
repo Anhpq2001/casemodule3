20c16d4192916935f24927beb8e4eaa38f36f082
Phạm Quang Anh <106795680+Anhpq2001@users.noreply.github.com>
2023-09-06 12:05:20 +0700

    add file

---
+++ src/main/java/connection/MyConnection.java
@@ -0,0 +1,31 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+    private static Connection connection;
+
+    public static Connection getInstance() {
+        if (connection == null) {
+            connection = getConnection();
+        }
+        return connection;
+    }
+
+    private static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String URL = "jdbc:mysql://localhost:3306/case_module3_1";
+            String username = "root";
+            String password = "1234567890";
+            connection = DriverManager.getConnection(URL, username, password);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+}
